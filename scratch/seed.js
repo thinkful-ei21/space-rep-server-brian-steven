52bf4e004ddb710271796acda93ea1161e06e820
Steven Imus <imussg@gmail.com>
2018-08-06 12:12:22 -0400

    files added

---
+++ scratch/seed.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { DATABASE_URL } = require('../config');
+
+const User = require('../models/user');
+
+const seedUsers = require('../db/seed/users');
+
+mongoose.connect(DATABASE_URL)
+	.then(() => mongoose.connection.db.dropDatabase())
+	.then(() => {
+		return Promise.all([
+			User.insertMany(seedUsers);
+		]);
+	})
+	.then(() => mongoose.disconnect())
+	.catch(err => {
+		console.log(err);
+	});
