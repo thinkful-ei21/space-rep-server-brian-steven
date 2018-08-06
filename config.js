788c170cbdabc16b9b9ed3bd5c01366e22ed0efb
Steven Imus <imussg@gmail.com>
2018-08-06 17:29:22 -0400

    set expiry global variable

--- config.js
+++ config.js
@@ -8,7 +8,8 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: 3600 * 1000
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
